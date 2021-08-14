4fc0bd041c6191def1ddda2cb118a7bf179731cf
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-14 22:34:58 +0100

    Add draft src

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,11 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const repository: string = core.getInput('repository')
+    const tag: string = core.getInput('tag')
+    core.debug(`${repository}:${tag}`)
+    core.setOutput('metadata', `${repository}:${tag}`)
   } catch (error) {
     core.setFailed(error.message)
   }
