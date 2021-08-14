071acd87428e138f8f4a8a579178a0bf71cee8ba
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-15 00:36:27 +0100

    Complete source code

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import * as core from '@actions/core'
+import {get_metadata} from './fetch-metadata'
 
 async function run(): Promise<void> {
   try {
     const repository: string = core.getInput('repository')
     const tag: string = core.getInput('tag')
-    core.debug(`${repository}:${tag}`)
-    core.setOutput('metadata', `${repository}:${tag}`)
+    const metadata = get_metadata(repository, tag)
+    core.setOutput('metadata', metadata)
   } catch (error) {
     core.setFailed(error.message)
   }
