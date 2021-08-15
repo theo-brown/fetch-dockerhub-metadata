e3d2142b148536553e6c1e2f4e34d65fade453e1
Theo Brown <7982453+theo-brown@users.noreply.github.com>
2021-08-15 10:23:56 +0100

    Expose JSON values as action outputs

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,21 @@
 import * as core from '@actions/core'
-import {get_metadata} from './fetch-metadata'
+import {get_metadata} from './metadata'
 
 async function run(): Promise<void> {
   try {
     const repository: string = core.getInput('repository')
     const tag: string = core.getInput('tag')
-    const metadata = get_metadata(repository, tag)
-    core.setOutput('metadata', metadata)
+    const metadata = await get_metadata(repository, tag)
+    core.setOutput('architecture', metadata.architecture)
+    core.setOutput('config', metadata.config)
+    core.setOutput('labels', metadata.config.Labels)
+    core.setOutput('container', metadata.container)
+    core.setOutput('container_config', metadata.container_config)
+    core.setOutput('created', metadata.created)
+    core.setOutput('docker_version', metadata.docker_version)
+    core.setOutput('history', metadata.history)
+    core.setOutput('os', metadata.os)
+    core.setOutput('rootfs', metadata.rootfs)
   } catch (error) {
     core.setFailed(error.message)
   }
